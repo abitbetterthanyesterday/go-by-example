67b09e7950514cc9e991941f477b8bb7db455b7e
Aloys Berger <aloys.berger@technichegroup.com>
2023-02-13 10:49:32 +1000

    feat(hello-world): Finish the first chapter, add languages (french, spanish)

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Alo");
-	want := "Hello, Alo"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Alo", "")
+		want := "Hello, Alo"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello to people in Spanish", func(t *testing.T) {
+		got := Hello("Alo", "Spanish")
+		want := "Hola, Alo"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say hello to people in French", func(t *testing.T) {
+		got := Hello("Alo", "French")
+		want := "Bonjour, Alo"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
 }
